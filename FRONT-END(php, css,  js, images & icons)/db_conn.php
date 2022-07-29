bf9f1ff9fb3e171445da7859a2db550b5cc1643b
ManishMalviya1 <82806226+ManishMalviya1@users.noreply.github.com>
2022-07-29 11:23:18 +0530

    Delete FRONT-END(php, css,  js, images & icons) directory

--- FRONT-END(php, css,  js, images & icons)/db_conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-		$conn = mysqli_connect("localhost", "root", 7665, "coledu");
-        if($conn === false){
-			die("ERROR: Could not connect. "
-				. mysqli_connect_error());
-		}
-		// mysqli_close($conn);
-?>
